351278f7b8ad3e188578bf21a450d4703cfdd6fe
Ayush Kumar <166158964+06Ayush-Kumar@users.noreply.github.com>
2024-10-16 19:40:56 +0530

    Update 1.Recursion.cpp

--- 1.Recursion.cpp
+++ 1.Recursion.cpp
@@ -17,9 +17,25 @@ int summation(int a){
     return 0;
     
 }
+void fun1(int a){
+    if(a>0){
+    cout<<a;
+    fun1(a-1);  // this is called tail recursion because after function there is nothing to execute  , fun1(a-1)+a will not be tail as +a is after function call which will execute
+    }
+}
+void fun2(int a){
+    if(a>0){
+    fun2(a-1);    // this is called head recursion because function calling is at top
+     cout<<a;
+    
+    }
+}
 
 int main() {
-	// your code goes here
-cout<<	factorial(5)<<"\n"<<summation(5);   // output :120 and  15
+	
+cout<<	factorial(5)<<"\n"<<summation(5)<<"\n";  // output :120 and  15
+fun1(5);   //output :54321
+cout<<"\n";
+fun2(5);   // output : 12345
 
 }
