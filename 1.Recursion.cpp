9f2a6e189219c06f30684bc78ff9158814ae3b1b
Ayush Kumar <166158964+06Ayush-Kumar@users.noreply.github.com>
2024-10-19 11:36:44 +0530

    Update 1.Recursion.cpp

--- 1.Recursion.cpp
+++ 1.Recursion.cpp
@@ -39,3 +39,44 @@ cout<<"\n";
 fun2(5);   // output : 12345
 
 }
+
+                                                                                         // solving taylor series using recursion
+
+
+#include<iostream>
+using namespace std;
+
+
+int pow(int x, int n) {     // Power function using integers for exponentiation
+    if (n == 0) {
+        return 1;
+    }
+    else {
+        return pow(x, n - 1) * x;
+    }
+}
+
+
+int fact(int n) {     // Factorial function using integers
+    if (n == 0) {
+        return 1;
+    }
+    else {
+        return fact(n - 1) * n;
+    }
+}
+
+
+float taylor(int x, int n) {   // Taylor series for e^x approximation
+    float sum = 0.0;  // Initialize sum for accumulating terms
+    for (int i = 0; i <= n; i++) {
+        sum += (float) pow(x, i) / fact(i);  // Accumulate each term in the series
+    }
+    return sum;  // Return the accumulated value of the Taylor series
+}
+
+int main() {
+    cout << taylor(1, 2);  // Call Taylor function for e^1 with 2 terms      taylor=summation of n(0 to inf)[power(x,n)/fact(n)]
+    return 0;
+}  // output 2.5
+
